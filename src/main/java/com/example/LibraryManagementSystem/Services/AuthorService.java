ab018314663da57a09d6e763e0e50809df37c227
priyesh37 <priyeshthakker3100@gmail.com>
2024-02-01 06:35:28 +0530

    add swagger and mail api

--- src/main/java/com/example/LibraryManagementSystem/Services/AuthorService.java
+++ src/main/java/com/example/LibraryManagementSystem/Services/AuthorService.java
@@ -4,6 +4,8 @@ import com.example.LibraryManagementSystem.DTO.AddAuthorRequest;
 import com.example.LibraryManagementSystem.Entities.Author;
 import com.example.LibraryManagementSystem.Repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +14,28 @@ public class AuthorService {
         @Autowired
         private AuthorRepository authorRepository;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
         public String addAuthor(AddAuthorRequest addAuthorRequest){
 
             Author authorEntity = new Author(addAuthorRequest.getAuthorName(),addAuthorRequest.getAuthorAge(),addAuthorRequest.getEmailId());
 
             Author newAuthor =  authorRepository.save(authorEntity);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setSubject("Hi "+newAuthor.getAuthorName()+" !");
+
+            message.setFrom("springacciojob@gmail.com");
+            message.setTo(newAuthor.getEmailId());
+
+
+            message.setText("You have been successfully Registered on our portal !" +
+                    "Looking forward for adding more books ");
+
+            mailSender.send(message);
+
 
             return "Author  with authorId "+newAuthor.getAuthorId()+" has been saved to DB";
         }
